a1989069cd34ef7ffcf68efe80398ea8dafbfa8c
prince <ibrunolisboa@icloud.com>
2026-08-23 19:25:52 -0300

    exercise redone

--- chapter3/exercise3.9.py
+++ chapter3/exercise3.9.py
@@ -1,8 +1,11 @@
 # turn days in hours, minutes and seconds
 days = int(input("Put the days here: "))
-hours = days*24
-minutes = hours*60
-seconds = minutes*60
-print(f" {days} days in hours are: {hours} hours")
-print(f" in minutes are: {minutes} minutes")
-print(f" in seconds are: {seconds} seconds")
+hours = int(input("Put the hours here: "))
+minutes = int(input("Put the minutes here: "))
+seconds = int(input("Put the seconds here: "))
+
+daysToSeconds = days * 24 * 60 * 60
+hoursToSeconds = hours * 60 * 60
+minutesToSeconds = minutes * 60
+
+print(f"{days} days {hours} hours {minutes} minutes and {seconds} seconds are {daysToSeconds + hoursToSeconds + minutesToSeconds + seconds} seconds")
