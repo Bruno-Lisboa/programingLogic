89a93639b84e89d18a3652a5939fbd57de0faa1d
prince <ibrunolisboa@icloud.com>
2026-08-24 16:31:34 -0300

    exercise 4.1 added

---
+++ chapter4/exercise4.1.py
@@ -0,0 +1,10 @@
+a = int(input("First value: "))
+b = int(input("Second value: "))
+
+if a > b:                                          #1
+    print("The first value is the highest!")       #2
+if b > a:                                          #3
+    print("The second value is the highest!")      #4
+
+# if the variables a and b have the same value, the condictions 1 and 3 won't read the block 2 and 4
+
