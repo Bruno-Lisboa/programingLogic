8f30c87740d6f629a31439c7fa68e686bee07fb7
prince <ibrunolisboa@icloud.com>
2026-08-27 19:23:26 -0300

    The exercise that made me grow my programming skills

--- chapter6/e6-3.py
+++ chapter6/e6-3.py
@@ -1,11 +1,8 @@
-# the exercise this time wanted to me to create a third list
-# with the elements of first and second list, but without repeat
-# the elements with the same values.
-# one more fail to me yay \o/
+# the exercise this time wanted to me to create a third list     
+# with the elements of first and second list, but without repeat the elements with the same values.
+# this exercise was really important to me. It showed that I can learn programming, I just have to give the time to learn.
 list1 = []
 list2 = []
-list3 = []
-spy = 0
 
 while True:
     addToList = int(input(f"add an element to the first list (0 when finished.): "))
@@ -13,7 +10,6 @@ while True:
         break
     else:
         list1.append(addToList)
-
 while True:
     addToList = int(input(f"add an element to the second list (0 when finished.): "))
     if addToList == 0:
@@ -21,8 +17,22 @@ while True:
     else:
         list2.append(addToList)
 
-while spy < len(list1):
-   list3.append(list1[spy]) 
-   spy += 1
+list3 = list1[:] + list2[:]
+finalList = []
+if len(list3) == 0:
+    print("You didn't add any element to the lists!")
+else:
+    finalList.append(list3[0])
+add = 0
+check = 0
 
-print(f"list1 {list1} list3 {list3}")
+while add < len(list3) and check < len(finalList):
+    if list3[add] == finalList[check]:
+        add += 1
+        check = 0
+    elif check == len(finalList) - 1:
+        finalList.append(list3[add])
+    else:
+        check += 1
+
+print(finalList)
